3fa45f6282f27e9b1c7cca74af55cc293f315fb9
Tanjil <tanjilkh@gmail.com>
2024-12-21 11:42:07 +0000

    api recipes

---
+++ server/data/recipes.js
@@ -0,0 +1,19 @@
+const recipes = [
+    {
+      _id: '1',
+      title: 'Bolo de Chocolate',
+      image: 'https://pt.petitchef.com/imgupl/recipe/bolo-de-chocolate-humido-e-fofinho--454177p704082.jpg'
+    },
+    {
+      _id: '2',
+      title: 'Salada Fresca',
+      image: 'https://cdn.recipes.lidl/images/pt-PT/Salada-fresca-de-verao.jpg',
+    },
+    {
+      _id: '3',
+      title: 'Sopa de Legumes',
+      image: 'https://assets.unileversolutions.com/recipes-v2/36850.jpg',
+    },
+];
+
+export default recipes;
