9f52115f488d23c5800e5a00df367ac156f9accc
Tanjil <tanjilkh@gmail.com>
2024-12-24 09:15:56 +0000

    api: ter proprias receitas

--- server/data/recipes.js
+++ server/data/recipes.js
@@ -2,17 +2,20 @@ const recipes = [
     {
       _id: '1',
       title: 'Bolo bueda bom',
-      image: 'https://pt.petitchef.com/imgupl/recipe/bolo-de-chocolate-humido-e-fofinho--454177p704082.jpg'
+      image: 'https://pt.petitchef.com/imgupl/recipe/bolo-de-chocolate-humido-e-fofinho--454177p704082.jpg',
+      user: "utilizador1"
     },
     {
       _id: '2',
       title: 'Salada Fresca teste',
       image: 'https://cdn.recipes.lidl/images/pt-PT/Salada-fresca-de-verao.jpg',
+      user: "utilizador2"
     },
     {
       _id: '3',
       title: 'Sopa de Legumes 123',
       image: 'https://assets.unileversolutions.com/recipes-v2/36850.jpg',
+      user: "utilizador1"
     },
 ];
 
